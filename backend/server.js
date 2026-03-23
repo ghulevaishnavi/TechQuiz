0f2da5e4d92b1825509364cd72df9e049d46d27c
vaishnavi ghule <ghulevaishzzz2004@gmail.com>
2026-03-23 17:10:58 +0530

    Update server logic

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,25 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
 import { connectDB } from './config/db.js';
 import userRoutes from './routes/user.js';
-const app = express();
-const PORT = process.env.PORT || 4000; // Added a backup port just in case
+import adminRoutes from './routes/admin.js';
 
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 // MIDDLEWARES
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
-// DB
-connectDB();
-
 // ROUTES
-app.use("/api/users", userRouter);
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
 
-app.get("/", (req, res) => {
-    res.send("API WORKING");
-});
+// DB CONNECTION
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server Started on http://localhost:${PORT}`);
